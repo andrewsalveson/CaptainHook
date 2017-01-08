9e8c74b696a18489f16a7acdc203e3d979e24821
Andrew Salveson <andrewsalveson@gmail.com>
2017-01-07 23:25:10 -0500

    made some changes

--- filter.sh
+++ filter.sh
@@ -2,25 +2,32 @@
 cat <<\CAPHOOK
 Scanning diff for modified files . . .
 CAPHOOK
-oldIFS=${IFS}
-declare -A assoc
-while IFS=, read -r -a array
-do 
-  ((${#array[@]} >= 2 )) || continue
-  assoc["${array[0]}"]="${array[@]:1}"
-done < ./map
-for key in "${!assoc[@]}"
-do
-  echo "${key} ---> ${assoc[${key}]}"
+
+
+git diff --cached --name-status | while read -r flag file ; do
+  if [ "$flag" == "M" ]
+  then
+    filetype=`echo "$file" | cut -d'.' -f2`
+    oldIFS=${IFS}
+    declare -A assoc
+    while IFS=, read -r -a array
+    do 
+      ((${#array[@]} >= 2 )) || continue
+      assoc["${array[0]}"]="${array[@]:1}"
+    done < ./map
+    for key in "${!assoc[@]}"
+    do
+      if [ "$filetype" == "${key}"   ]
+      then
+        # echo "$file is a ${key} ---> ${assoc[${key}]}"
+        echo ${assoc[$key]}
+        .git/caphook/handler.sh "${assoc[$key]}" $file
+      fi
+    done
+    IFS=${oldIFS}
+  fi;
 done
-IFS=${oldIFS}
 
-for blah in $(git diff --cached --name-status); do
-  echo "checking $blah"
-done
-
-  git diff --cached --name-status | awk '$1 == "M" { print "$2 was modified" }'
-
-
+  # git diff --cached --name-status | awk '$1 == "M" { print "$2 was modified" }'
 
 #END caphook
