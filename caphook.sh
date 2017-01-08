1451fa6a88e49bc708114bda1d262cb4cd885c6f
Andrew Salveson <andrewsalveson@gmail.com>
2017-01-08 00:33:14 -0500

    a bloo blaa blee

--- caphook.sh
+++ caphook.sh
@@ -14,7 +14,47 @@ echo "file type: $fileType"
 echo "handler script: $handlerScript"
 
 install() {
-  payload=$(<filter.sh)
+  # payload=$(<filter.sh)
+  payload=$(cat<<-'FILTER'
+#BEGIN caphook
+cat <<\CAPHOOK
+Scanning diff for modified files . . .
+CAPHOOK
+
+commit=$(git rev-parse HEAD)
+
+echo "commit $commit"
+
+git diff "$commit^" "$commit" --name-status | while read -r flag file ; do
+  if [ "$flag" == "M" ]
+  then
+    filetype=`echo "$file" | cut -d'.' -f2`
+    oldIFS=${IFS}
+    declare -A assoc
+    while IFS=, read -r -a array
+    do 
+      ((${#array[@]} >= 2 )) || continue
+      assoc["${array[0]}"]="${array[@]:1}"
+    done < .git/caphook/map
+    for key in "${!assoc[@]}"
+    do
+      echo "check $filetype against ${key}"
+      if [ "$filetype" == "${key}"   ]
+      then
+        echo "$file is a ${key} ---> ${assoc[${key}]}"
+        echo ${assoc[$key]}
+        .git/caphook/handler.sh "${assoc[$key]}" $file
+      fi
+    done
+    IFS=${oldIFS}
+  fi;
+done
+
+  # git diff --cached --name-status | awk '$1 == "M" { print "$2 was modified" }'
+
+#END caphook
+FILTER
+)
   if [ -f "$prepush" ]
   then
 	echo "$prepush already exists"
@@ -37,7 +77,26 @@ install() {
   mkdir "$caphookPath/temp"
   echo "made the $caphookPath/temp folder"
   fi
-  cp handler.sh "$caphookPath/"
+  cat << HANDLER > "$caphookPath/handler.sh"
+#!/bin/sh
+url=${1%$'\r'}
+file=$2
+filetype=`echo "$file" | cut -d'.' -f2`
+git show HEAD~1:$file > .git/caphook/temp/old.$filetype
+case $filetype in
+  gh)
+    url="$url/gh" ;;
+  osm) 
+    url="$url/osm" ;;
+  \?)
+    ;;
+esac
+curl \
+  -F "model=@.git/caphook/temp/old.$filetype" \
+  -F "compare=@$file" \
+  "$url" > output.html
+HANDLER
+  # cp handler.sh "$caphookPath/"
   if ! [ -d "$filesPath" ]; then
 	mkdir $filesPath
 	echo "made the $filesPath folder"
